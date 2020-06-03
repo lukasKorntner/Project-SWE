6d1ae2856aa3b4b2401251087a9b3aec352c8f6e
lukasKorntner <lukas.korntner@icloud.com>
2020-06-03 19:15:28 +0000

    Add existing file

---
+++ getrss.php
@@ -0,0 +1,42 @@
+<?php
+
+
+//find out which feed was selected
+
+  $xml=("https://rss.orf.at/news.xml");
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load($xml);
+
+//get elements from "<channel>"
+$channel=$xmlDoc->getElementsByTagName('channel')->item(0);
+$channel_title = $channel->getElementsByTagName('title')
+->item(0)->childNodes->item(0)->nodeValue;
+$channel_link = $channel->getElementsByTagName('link')
+->item(0)->childNodes->item(0)->nodeValue;
+$channel_desc = $channel->getElementsByTagName('description')
+->item(0)->childNodes->item(0)->nodeValue;
+
+
+//get and output "<item>" elements
+$x=$xmlDoc->getElementsByTagName('item');
+for ($i=0; $i<=2; $i++) {
+  $item_title=$x->item($i)->getElementsByTagName('title')
+  ->item(0)->childNodes->item(0)->nodeValue;
+  $item_link=$x->item($i)->getElementsByTagName('link')
+  ->item(0)->childNodes->item(0)->nodeValue;
+  echo ("<p>". $item_title);
+  echo ("<br>");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>RSS-Reader</title>
+</head>
+<body>
+<item></item>
+</body>
+</html>
+
